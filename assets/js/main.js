05df2b300758644f3447d3c9d36de056c3bae9aa
Yeison Serrano <ydanielsb97@gmail.com>
2023-12-04 07:07:23 -0400

    ✨ Parallax, scroll reveal and animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,15 +39,57 @@ window.addEventListener('scroll', scrollHeader)
 
 
 /*=============== SHOW SCROLL UP ===============*/ 
-
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
 
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== PARALLAX ===============*/
-
+const rellax = new Rellax('.parallax')
 
 /*=============== GSAP ANIMATION ===============*/
-
+gsap.from('.home__village', 1.2, {opacity: 0, y: 100, delay: .1})
+gsap.from('.home__pine', 1.2, {opacity: 0, y: 150, delay: .3})
+gsap.from('.home__mountain-2', 1.2, {opacity: 0, x: 150, delay: .5})
+gsap.from('.home__mountain-3', 1.2, {opacity: 0, x: -150, delay: .6})
+gsap.from('.home__mountain-1', 1.2, {opacity: 0, y: 250, delay: .7})
+gsap.from('.home__moon', 1.2, {opacity: 0, y: 200, delay: .8})
+gsap.from('.home__trineo', 1.2, {opacity: 0, x: -200, delay: .8})
+gsap.from('.home__title', 1.2, {opacity: 0, x: -60, delay: .1})
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400
+})
+
+sr.reveal('.about__data, .celebrate__img', {origin: 'right'})
+sr.reveal('.about__img, .celebrate__data', {origin: 'left'})
+sr.reveal('.about__card', {interval: 100})
+sr.reveal('.footer')
